42b68098667e146b428f218de825e98aa070c675
Vincentius Ludwig <vnako22@gmail.com>
2024-02-03 19:23:22 +0700

    Add files via upload

---
+++ src/main/java/com/example/dssproject/testbackend/controller/KaryawanController.java
@@ -0,0 +1,43 @@
+package com.example.dssproject.testbackend.controller;
+
+
+import com.example.dssproject.testbackend.entity.KaryawanEntity;
+import com.example.dssproject.testbackend.service.KaryawanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/karyawan")
+public class KaryawanController {
+    @Autowired
+    private KaryawanService karyawanService;
+
+    @GetMapping
+    public List<KaryawanEntity> getAllUser(){
+        List<KaryawanEntity> karyawan = karyawanService.getAllUser();
+        System.out.println("users : "+karyawan);
+        return karyawan;
+    }
+
+    @GetMapping("/{karyawanId}")
+    public KaryawanEntity getUserById(@PathVariable Integer karyawanId){
+        KaryawanEntity user = karyawanService.getUserById(karyawanId);
+        System.out.println("userId : "+karyawanId+" : user : "+user);
+        return user;
+    }
+
+    @PostMapping
+    public String saveUser(@RequestBody KaryawanEntity user){
+        karyawanService.saveUser(user);
+        return "user saved successfuly.";
+    }
+
+    @DeleteMapping("/{KaryawanId}")
+    public String deleteKaryawanId(@PathVariable Integer KaryawanId){
+        karyawanService.deleteKaryawanId(KaryawanId);
+        return "user deleted successfully.";
+    }
+
+}
---
+++ src/main/java/com/example/dssproject/testbackend/service/KaryawanService.java
@@ -0,0 +1,16 @@
+package com.example.dssproject.testbackend.service;
+
+import com.example.dssproject.testbackend.entity.KaryawanEntity;
+
+import java.util.List;
+
+public interface KaryawanService {
+    List<KaryawanEntity> getAllUser();
+
+    KaryawanEntity getUserById(Integer userId);
+
+    void saveUser(KaryawanEntity user);
+
+    void deleteKaryawanId(Integer userId);
+
+}
